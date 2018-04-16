fcde4332ae5aec885cf236e6d3d63104de044507
Keshav Bansal <keshavbans@gmail.com>
2018-04-16 13:52:14 +0100

    import ERC721

--- zombieownersip.sol
+++ zombieownersip.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.4.19;
 
 import "./zombieattack.sol";
+// Import file here
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+// Declare ERC721 inheritance here
+contract ZombieOwnership is ZombieAttack, ERC721 {
 
 }
